82b6dac901c62b988b65adaedbd4dc328a973061
ZeroneOffcial <rafliwidyanata879@gmail.com>
2025-11-26 21:53:18 +0700

    Update Telegram API credentials in settings.py
    Updated API_ID and API_HASH with new values.

--- settings.py
+++ settings.py
@@ -9,5 +9,5 @@ PLTC = "ptlc_YauSzZjWSSWJFFy8HWKvWaas961QbiQ46qFMcLGbSci"
 LOC = 1
 EGGS = 15
 # === TELEGRAM API (my.telegram.org) ===
-API_ID =  # ganti dengan api yang sesuai 
-API_HASH = "" # ----"-----
+API_ID = 27095838 # ganti dengan api yang sesuai 
+API_HASH = "28c51c248482f0ca68d091397e2e491b" # ----"-----
