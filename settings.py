e9cbd4ea5a3defd96f56dae8c1cab9df2d8ad2eb
ZeroneOffcial <rafliwidyanata879@gmail.com>
2025-11-26 21:52:40 +0700

    Update OWNER_ID and BOT_TOKEN in settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # === TELEGRAM OWNER ===
-OWNER_ID = 7868229705
+OWNER_ID = 7716690774
 PP = "https://files.catbox.moe/zzx0s5.jpg"
-BOT_TOKEN = "8193607838:AAG3skaKfEuFNkA79s_HQyj-jFMrs15Gb7k"
+BOT_TOKEN = "7998603978:AAEjEITbH3hDYHrejz11jmDWjQFyjqVt6Ac"
 # === PTERODACTYL PANEL ===
 DOMAIN = "YourDomain"
 PLTA = "ptla_ZeiAUwnqme299jhutwvbKYpOS0AWDUORRKJQtG4qbdR"
